28c2c11e821755bc2bf7ce36a91aa3a5495cb3ad
Sivadinesh M <sivadinesh@gmail.com>
2021-09-19 01:48:10 +0530

    interim 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,9 @@
 import {
-	Component,
-	ViewChild,
-	OnInit,
-	ChangeDetectionStrategy,
-	ChangeDetectorRef,
+  Component,
+  ViewChild,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
 } from '@angular/core';
 import { LoadingService } from '../components/loading/loading.service';
 import { MessagesService } from '../components/messages/messages.service';
@@ -13,35 +13,49 @@ import { Router } from '@angular/router';
 import { SidenavService } from '../services/sidenav.service';
 
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.page.html',
-	styleUrls: ['./home.page.scss'],
-	providers: [LoadingService, MessagesService],
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  providers: [LoadingService, MessagesService],
 })
 export class HomePage implements OnInit {
-	userdata: any;
-	center_id: any;
+  userdata: any;
+  center_id: any;
 
-	onSideNavChange: boolean;
+  onSideNavChange: boolean;
 
-	constructor(
-		private _authservice: AuthenticationService,
-		private _sidenavService: SidenavService,
-		private _router: Router
-	) {
-		this._sidenavService.sideNavState$.subscribe((res) => {
-			this.onSideNavChange = res;
-		});
-	}
+  isMenuOpen = true;
+  contentMargin = 180;
 
-	ngOnInit() {}
+  constructor(
+    private _authservice: AuthenticationService,
+    private _sidenavService: SidenavService,
+    private _router: Router
+  ) {
+    this._sidenavService.sideNavState$.subscribe((res) => {
+      this.onSideNavChange = res;
+    });
+  }
 
-	goAdmin() {
-		this._router.navigate([`/home/admin`]);
-	}
+  ngOnInit() {}
 
-	async logout() {
-		await this._authservice.logOut();
-		this._router.navigateByUrl('');
-	}
+  onToolbarMenuToggle() {
+    console.log('On toolbar toggled', this.isMenuOpen);
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (!this.isMenuOpen) {
+      this.contentMargin = 60;
+    } else {
+      this.contentMargin = 180;
+    }
+  }
+
+  goAdmin() {
+    this._router.navigate([`/home/admin`]);
+  }
+
+  async logout() {
+    await this._authservice.logOut();
+    this._router.navigateByUrl('');
+  }
 }
